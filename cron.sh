afe17954b349f6e66858ff0410a869d4e7bcde99
kennsippell <kenn@dotacoach.org>
2018-01-12 21:46:51 -0800

    Cron job should include console output in email

--- cron.sh
+++ cron.sh
@@ -3,4 +3,4 @@ cd /home/ec2-user/arrbitrage
 git pull
 nvm use v8.9.4
 npm i
-~/.nvm/versions/node/v8.9.4/bin/node simulation real >> /home/ec2-user/arrbitrage/cron.log
+~/.nvm/versions/node/v8.9.4/bin/node simulation real
